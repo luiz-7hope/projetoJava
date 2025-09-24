ad0b7707fbf93144a4b1732ddde9d523101d8b89
luiz-7hope <luiz.lem6@gmail.com>
2025-09-24 18:55:12 -0300

    Corrigi bug no método verificarVencedor

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -73,21 +73,18 @@ public class JogoDaVelha {
     }
 
     static boolean verificarVitoria() {
-        // Verificar linhas
         for (int i = 0; i < 3; i++) {
             if (tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) {
                 return true;
             }
         }
 
-        // Verificar colunas
         for (int j = 0; j < 3; j++) {
             if (tabuleiro[0][j] == jogadorAtual && tabuleiro[1][j] == jogadorAtual && tabuleiro[2][j] == jogadorAtual) {
                 return true;
             }
         }
 
-        // Verificar diagonais
         if (tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
             return true;
         }
